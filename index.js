fbc702a466895f9edc200d9a11b47db221741605
IvanKarpets <karpecivan304gmail.com>
2026-09-14 16:40:17 +0300

    add import resend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pool = require('./db');
 const { logInfo, logError } = require('./logger');
 const { PubSub } = require('@google-cloud/pubsub');
+const { Resend } = require('resend');
 
 
 const pubsub = new PubSub();
